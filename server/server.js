a90d3eede97c4c32cca593ecb0e151c6c85380bb
ProfessionalReally <nikita22408@gmail.com>
2025-04-09 17:09:57 +0700

    chore: add dotenv for environment variables

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('API IS RUNNING!')
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+app.listen(PORT, () => {
+    console.log(`Example app listening on port ${PORT}`)
 })
