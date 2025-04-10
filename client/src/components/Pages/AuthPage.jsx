c03a6d73e32dd5eb28082f81e1dfe6c2e20b2ce6
ProfessionalReally <nikita22408@gmail.com>
2025-04-10 15:54:48 +0700

    feat(auth): add AuthPage component with basic structure

---
+++ client/src/components/Pages/AuthPage.jsx
@@ -0,0 +1,60 @@
+import {Container, Flex, Paper, Tabs, Title} from "@mantine/core";
+export default function AuthPage() {
+
+    return (
+        <Container mx='auto' size={480}
+        >
+            <Flex
+                mih={50}
+                mt={40}
+                gap="md"
+                justify="center"
+                align="center"
+                direction="column"
+                wrap="wrap"
+            >
+                <Paper bg='#141414'
+                       p='xl'
+                       bd='1px solid #2e2e2e'
+                       shadow="md"
+                       radius="lg"
+                       w='100%'
+                >
+                    <Flex align='center' justify='center'>
+                        <Title order={1} c='#fff'>
+                            Chat Real-Time
+                        </Title>
+                    </Flex>
+                </Paper>
+                <Paper
+                    bd='1px solid #2e2e2e'
+                    shadow="md"
+                    radius="lg"
+                    bg='#141414'
+                    p='xl'
+                    w='100%'
+                >
+                    <Tabs variant="pills" radius="lg" defaultValue="login">
+                        <Tabs.List mb='xs' grow justify="flex-start">
+                            <Tabs.Tab value="login">
+                                Login
+                            </Tabs.Tab>
+                            <Tabs.Tab value="sign-up">
+                                Sign Up
+                            </Tabs.Tab>
+                        </Tabs.List>
+                        <Tabs.Panel value="login">
+                            Login tab content
+                        </Tabs.Panel>
+
+                        <Tabs.Panel value="sign-up">
+                            Sign Up tab content
+                        </Tabs.Panel>
+                    </Tabs>
+                </Paper>
+            </Flex>
+
+        </Container>
+    )
+}
+
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { MantineProvider } from '@mantine/core';
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
+import {createTheme, MantineProvider} from '@mantine/core';
 import './index.css'
 import App from './App.jsx'
+import '@mantine/core/styles.css';
+
+const customTheme = createTheme({
+        fontFamily: 'Work Sans, sans-serif',
+    }
+);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-      <MantineProvider>
-          <App />
-      </MantineProvider>
-  </StrictMode>,
+    <StrictMode>
+        <MantineProvider theme={customTheme} defaultColorScheme="dark">
+            <App/>
+        </MantineProvider>
+    </StrictMode>,
 )
